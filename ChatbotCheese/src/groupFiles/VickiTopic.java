32c9b3d1a020aba5f2fb9c109a08ff27f891e5b8
Student6 <Student6@10.8.33.107>
2016-10-05 13:10:55 -0400

    Update

--- ChatbotCheese/src/groupFiles/VickiTopic.java
+++ ChatbotCheese/src/groupFiles/VickiTopic.java
@@ -1,14 +1,34 @@
 package groupFiles;
-
 public class VickiTopic implements Topic {
+	private boolean inVickiLoop;
+	private String vickiResponse;
 
 	@Override
 	public void talk() {
-		// TODO Auto-generated method stub
-		
+		VickiMain.print("What kind of food do you want to make? Breakfast, lunch, dinner, or dessert?");
+		inVickiLoop = true;
+		while(inVickiLoop) {
+			findFood();
+		}
+	}
+	public void findFood() {
+		vickiResponse = VickiMain.getInput();
+		String[] foods = {"breakfast", "lunch", "dinner", "dessert"};
+		boolean foodFound = false;
+		for (int i = 0; i < foods.length; i++) {
+			if (VickiMain.findKeyword(vickiResponse, foods[i], 0) >= 0) {
+				int foodPsn = VickiMain.findKeyword(vickiResponse, foods[i], 0);
+				if (foodPsn >= 0) {
+					VickiMain.print(foods[i]);
+					foodFound = true;
+				}
+			}
+		}
+		if (!foodFound) {
+			VickiMain.print("I don't understand");
+		}
 	}
 
-	@Override
 	public boolean isTriggered(String userInput) {
 		String[] triggers = {"make", "cook", "recipe"};
 		for (int i = 0; i < triggers.length; i++) {
