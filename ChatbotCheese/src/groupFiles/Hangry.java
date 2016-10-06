89f2f0e25e9edafbc870e93bc9f5b39064692abf
Student8 <Student8@10.8.33.151>
2016-10-06 12:31:40 -0400

    has basic code now

--- ChatbotCheese/src/groupFiles/Hangry.java
+++ ChatbotCheese/src/groupFiles/Hangry.java
@@ -3,17 +3,28 @@ package groupFiles;
 import groupFiles.VickiMain;
 
 public class Hangry implements Topic{
+	private boolean imHangryLoop;
+	private String feedMeResponse;
 
 	@Override
 	public void talk() {
-		
-		
+		imHangryLoop = true;
+		while(imHangryLoop){
+			VickiMain.print("I'm hungry! Feed me!");
+			feedMeResponse = VickiMain.getInput();
+			if(feedMeResponse.indexOf("stop") >= 0 );{
+				imHangryLoop =false;
+				VickiMain.talkForever();
+			}
+			VickiMain.print("Not Enough! FEED ME MORE FOOD!!");
+		}
 	}
 
 	@Override
 	public boolean isTriggered(String userInput) {
-		if (VickiMain,findKeyword(user))
+		// TODO Auto-generated method stub
+		return false;
 	}
 	
-
 }
+
